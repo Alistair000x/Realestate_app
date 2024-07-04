0a4d35b70e38e35c80f8849787e2dd41b217b1f5
Alistair000x <darmensun43@gmail.com>
2024-07-04 01:05:44 +0100

    yes

--- api/app.js
+++ api/app.js
@@ -8,19 +8,39 @@ import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
+// Create an instance of the Express application
 const app = express();
 
+// Use CORS middleware to allow requests from the client URL
+// Credentials: true allows cookies to be sent
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+// Middleware to parse JSON bodies
 app.use(express.json());
+
+// Middleware to parse cookies
 app.use(cookieParser());
 
+// Define route handlers
+// Authentication routes
 app.use("/api/auth", authRoute);
+
+// User routes
 app.use("/api/users", userRoute);
+
+// Post routes
 app.use("/api/posts", postRoute);
+
+// Test routes
 app.use("/api/test", testRoute);
+
+// Chat routes
 app.use("/api/chats", chatRoute);
+
+// Message routes
 app.use("/api/messages", messageRoute);
 
+// Start the server and listen on port 8800
 app.listen(8800, () => {
   console.log("Server is running!");
 });
